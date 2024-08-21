c1849030d4c06855b5b92cad4689800f98504dfb
Lenni009 <guentherhanslphdyt@gmail.com>
2024-08-21 21:50:56 +0200

    update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { fileURLToPath } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/nmscolorparser-v2/',
   plugins: [vue()],
   resolve: {
     alias: {
